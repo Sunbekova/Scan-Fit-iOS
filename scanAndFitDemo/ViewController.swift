fc9feb75b9a79a9a639bb977edcf6e117b1e997c
Sunbekova <suanbekova05@gmail.com>
2026-04-08 00:05:56 +0500

    Initial Commit

---
+++ scanAndFitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scanAndFitDemo
+//
+//  Created by Aisha Suanbekova Bakytjankyzy on 08.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
